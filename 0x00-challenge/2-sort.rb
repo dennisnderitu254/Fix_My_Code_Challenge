7c91a6b98f5d2070bc58b578a0ed67d47c42f3bd
dennisnderitu254 <dknderitu@gmail.com>
2023-02-13 16:01:55 +0300

    debugging the implementation of sorting arguments in Ruby

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,8 +1,18 @@
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
+
+# 1. It creates an empty array called result.
+# 2. It loops through each argument in ARGV.
+# 3. If the argument is not an integer, it skips to the next argument.
+# 4. If the argument is an integer, it converts it to an integer and stores it in a variable called i_arg.
+# 5. It then loops through each element in result.
+# 6. If the element is less than i_arg, it moves on to the next element.
+# 7. If the element is greater than or equal to i_arg, it inserts i_arg at that position and breaks out of the loop.
+# 8. If it gets to the end of result and hasn’t inserted i_arg, it appends i_arg to the end of result.
+# 9. It then prints out result.
 
 result = []
 ARGV.each do |arg|
@@ -11,7 +21,7 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -20,7 +30,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
