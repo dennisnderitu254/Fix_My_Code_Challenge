3ccf476cf90883ebff50b578ebe1c444bd7776a6
dennisnderitu254 <dknderitu@gmail.com>
2023-02-13 15:50:57 +0300

    changing square print from 16 to 10 in parseInt

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
 
+/*
+1. Check if the number of arguments is less than or equal to 2.
+2.  If it is, print an error message and exit with a status of 1.
+3.  Otherwise, convert the argument to an integer and store it in the size variable.
+4.  Loop through the rows.
+5.  Loop through the columns.
+6.  Print a # character.
+7.  Print a newline character.
+*/
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -14,10 +23,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
